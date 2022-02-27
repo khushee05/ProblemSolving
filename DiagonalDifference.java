895b07a7c9c9d9b0cd62ef188e32db0037e16471
Khushee Jain <78522883+khushee05@users.noreply.github.com>
2022-02-27 14:56:54 +0530

    Create DiagonalDifference.java

---
+++ DiagonalDifference.java
@@ -0,0 +1,110 @@
+
+/*
+iven a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+For example, the square matrix  is shown below:
+
+1 2 3
+4 5 6
+9 8 9  
+The left-to-right diagonal = . The right to left diagonal = . Their absolute difference is .
+
+Function description
+
+Complete the  function in the editor below.
+
+diagonalDifference takes the following parameter:
+
+int arr[n][m]: an array of integers
+Return
+
+int: the absolute diagonal difference
+Input Format
+
+The first line contains a single integer, , the number of rows and columns in the square matrix .
+Each of the next  lines describes a row, , and consists of  space-separated integers .
+
+Constraints
+
+Output Format
+
+Return the absolute difference between the sums of the matrix's two diagonals as a single integer.
+
+Sample Input
+
+3
+11 2 4
+4 5 6
+10 8 -12
+Sample Output
+
+15
+*/
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+  
+    public static int diagonalDifference(List<List<Integer>> arr) {
+    // Write your code here
+    int sum =0;
+    int leftSum =0; int rightSum =0;
+    int n = arr.size();
+    for(int i =0; i<n; i++){
+        for(int j =0; j<n;j++){
+            if( i==j){
+                leftSum += arr.get(i).get(j);
+                System.out.println(leftSum + " "+ arr.get(i).get(j));
+            }
+             if(n-i-1==j){
+                rightSum += arr.get(i).get(j); 
+                 System.out.println(rightSum + " "+ arr.get(i).get(j));
+            }
+        }
+    }
+    sum = Math.abs(leftSum-rightSum);
+    return sum;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, n).forEach(i -> {
+            try {
+                arr.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = Result.diagonalDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
